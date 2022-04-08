1614d6933e210a8b1f3bda16fbfac2c169cd4dc9
tohrnii <100405913+tohrnii@users.noreply.github.com>
2022-04-08 05:17:32 +0000

    add openlab exchange subgraph

---
+++ src/mapping.ts
@@ -0,0 +1,52 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  Exchange,
+  Received,
+  jobActive,
+  jobCancelled,
+  jobClosed,
+  jobCreated
+} from "../generated/Exchange/Exchange"
+
+import { Job } from "../generated/schema"
+
+export function handlejobActive(event: jobActive): void {
+  let id = event.params._jobId.toHex()
+  let job = Job.load(id)
+  if (job == null) {
+    job = new Job(id)
+  }
+  job.provider = event.params._provider
+  job.status = event.params._status
+  job.save()
+}
+
+export function handlejobCancelled(event: jobCancelled): void {
+  let id = event.params._jobId.toHex()
+  let job = Job.load(id)
+  if (job == null) {
+    job = new Job(id)
+  }
+  job.status = event.params._status
+  job.save()
+}
+
+export function handlejobClosed(event: jobClosed): void {
+  let id = event.params._jobId.toHex()
+  let job = Job.load(id)
+  if (job == null) {
+    job = new Job(id)
+  }
+  job.status = event.params._status
+  job.save()
+}
+
+export function handlejobCreated(event: jobCreated): void {
+  let job = new Job(event.params._jobId.toHex())
+  job.client = event.params._client
+  job.payableToken = event.params._payableToken
+  job.jobCost = event.params._jobCost
+  job.jobURI = event.params._jobURI
+  job.status = event.params._status
+  job.save()
+}
